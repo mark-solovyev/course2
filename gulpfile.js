b35c964ca3ee3a5cabd12a28bdc8ba5460c78608
mark-solovyev <mark-solovyev@gmail.com>
2019-06-06 16:03:16 +0300

    Installed gulp-sass and gulp-sass-glub

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 const gulp = require("gulp");
 const rm = require("gulp-rm");
-
-gulp.task("remove", () => {
+const sass = require("gulp-sass");
+sass.compiler = require("node-sass");
+const sassGlob = require("gulp-sass-glob");
+gulp.task("remove:html", () => {
     return gulp.src("./dist/**/*", {read: false})
     .pipe(rm());
 });
 
-gulp.task("copy", () => {
+gulp.task("copy:html", () => {
     return gulp.src("./src/*.html")
     .pipe(gulp.dest("./dist"));
 });
+
+gulp.task("sass", () => {
+    return gulp.src("./src/styles/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./dist/styles"));
+});
+
+gulp.task("default", gulp.series("remove:html", "copy:html", "sass"));
