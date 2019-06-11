16d08af9f1383f3efdde1bc4407db4bbd8c94dc2
mark-solovyev <mark-solovyev@gmail.com>
2019-06-11 22:02:52 +0300

    The first pages continues

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,14 @@ const sass = require("gulp-sass");
 sass.compiler = require("node-sass");
 const sassGlob = require("gulp-sass-glob");
 gulp.task("remove:html", () => {
-    return gulp.src("./dist/**/*", {read: false})
+    // remove all files exept fonts folder
+    return gulp.src(["./dist/**/*", "!./dist/fonts"])
     .pipe(rm());
+});
+
+gulp.task("copy:img", () => {
+    return gulp.src("./src/images/*.png")
+    .pipe(gulp.dest("./dist/images"));
 });
 
 gulp.task("copy:html", () => {
@@ -15,8 +21,9 @@ gulp.task("copy:html", () => {
 
 gulp.task("sass", () => {
     return gulp.src(["./src/styles/main.scss"])
+    //.pipe(sass().on("error", sass.logError))
     .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest("./dist/styles"));
 });
 
-gulp.task("default", gulp.series("remove:html", "copy:html", "sass"));
+gulp.task("default", gulp.series("remove:html","copy:html","copy:img","sass"));
